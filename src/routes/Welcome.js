08683d0adba67cee876e39540f58ab00ac9b3824
Adrian Perdomo <82109017+aperdomoll90@users.noreply.github.com>
2021-06-07 00:37:01 -0400

    link added to floatin island and fixed possitioning of the sun

--- src/routes/Welcome.js
+++ src/routes/Welcome.js
@@ -9,7 +9,7 @@ function Welcome() {
   let history = useHistory()
   return (
     <div className='main '>
-      <section className='animationField'>
+      <section onClick={() => history.push('/feelings')} className='animationField'>
         <div className='startButton'></div>
         <section className='FloatingBase'>
           <div className='FloatingBase-field'></div>
@@ -22,14 +22,14 @@ function Welcome() {
               <div className='lefMountain-mountain'></div>
               <img src={leftSnow} className='lefMountain-snow' />
             </section>
-            <div className="middleBush">
+            <div className='middleBush'>
               <div className='middleBush-circle'></div>
               <div className='middleBush-circle'></div>
               <div className='middleBush-circle'></div>
               <div className='middleBush-circle'></div>
               <div className='middleBush-base'></div>
             </div>
-            <div className="leftBush">
+            <div className='leftBush'>
               <div className='leftBush-circle'></div>
               <div className='leftBush-circle'></div>
               <div className='leftBush-circle'></div>
@@ -37,23 +37,23 @@ function Welcome() {
               <div className='leftBush-base'></div>
             </div>
 
-            <div className="pine">
-            <div className="pine-foliage"></div>
-            <div className="pine-foliage"></div>
-            <div className="pine-foliage"></div>
-            <div className="pine-foliage"></div>
-            <div className="pine-foliage"></div>
-            <div className="pine-foliage"></div>
-            <div className="pine-trunk"></div>
+            <div className='pine'>
+              <div className='pine-foliage'></div>
+              <div className='pine-foliage'></div>
+              <div className='pine-foliage'></div>
+              <div className='pine-foliage'></div>
+              <div className='pine-foliage'></div>
+              <div className='pine-foliage'></div>
+              <div className='pine-trunk'></div>
             </div>
-            <div className="rightPine">
-            <div className="pine-foliage"></div>
-            <div className="pine-foliage"></div>
-            <div className="pine-foliage"></div>
-            <div className="pine-foliage"></div>
-            <div className="pine-foliage"></div>
-            <div className="pine-foliage"></div>
-            <div className="pine-trunk"></div>
+            <div className='rightPine'>
+              <div className='pine-foliage'></div>
+              <div className='pine-foliage'></div>
+              <div className='pine-foliage'></div>
+              <div className='pine-foliage'></div>
+              <div className='pine-foliage'></div>
+              <div className='pine-foliage'></div>
+              <div className='pine-trunk'></div>
             </div>
 
             <section className='middleMountain'>
@@ -66,6 +66,21 @@ function Welcome() {
               <div className='rightMountain-mountain'></div>
               <img src={leftSnow} className='rightMountain-snow' />
             </section>
+            <div className='bigRock'>
+              <div className='bigRock-borderTop'>
+                <div className='bigRockBottom'><div className='bigRockBottom-shine'></div></div>
+              </div>
+            </div>
+            <div className='mediumRock'>
+              <div className='mediumRock-borderTop'>
+                <div className='mediumRockBottom'><div className='mediumRockBottom-shine'></div></div>
+              </div>
+            </div>
+            <div className='smallRock'>
+              <div className='smallRock-borderTop'>
+                <div className='smallRockBottom'><div className='smallRockBottom-shine'></div></div>
+              </div>
+            </div>
 
             <section className='cloud'>
               <div className='cloud-circle'></div>
